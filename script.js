9f16218dfdec0e4cca6ec3438647c840df2cecda
kumokuro <katieannemaher@gmail.com>
2021-11-23 15:57:30 -0600

    added dynamic grid and slider

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 const container = document.querySelector("#container");
 
+// display slider value / get gridSize as an int
+const slider = document.querySelector(".slider");
+const output = document.querySelector(".sizeText");
+output.textContent = slider.value;
+let gridSize = 16;
+
+slider.oninput = function () {
+  output.textContent = this.value;
+  gridSize = parseInt(slider.value);
+  reset();
+};
+
 // initiates the first grid
-createGrid();
+createGrid(gridSize);
 addColor();
 
-// creates 16 divs for the grid
-function createGrid() {
-  for (i = 0; i < 16; i++) {
+// creates the grid
+function createGrid(gridSize) {
+  container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+
+  for (i = 0; i < gridSize * gridSize; i++) {
     const grid = document.createElement("div");
     grid.classList.add("gridSquare");
     container.appendChild(grid);
@@ -31,6 +46,6 @@ function reset() {
   while (container.firstChild) {
     container.removeChild(container.lastChild);
   }
-  createGrid();
+  createGrid(gridSize);
   addColor();
 }
